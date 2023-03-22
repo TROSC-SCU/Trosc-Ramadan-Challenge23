72b062b409c2ef7aa8f2c68df953da1d90543d06
TROSC تروس <128643049+TROSC-SCU@users.noreply.github.com>
2023-03-22 23:11:22 +0200

    DAY 01

---
+++ Challeges/SubString.py
@@ -0,0 +1,14 @@
+# This function checks if the input string is a subarray of the word "Trosc" or not
+def TroscCheck():
+    s = "Trosc"
+    c = input()
+    if c in s:
+        return True
+    else:
+        return False
+
+# This code is not working, can you help me fixing it?
+if TroscCheck():
+print("YES")
+else
+print("NO")
