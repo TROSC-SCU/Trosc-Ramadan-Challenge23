bda33452ad4113372e48952b5d9fa2dc435ae9b6
TROSC تروس <128643049+TROSC-SCU@users.noreply.github.com>
2023-03-22 23:55:49 +0200

    DAY 01

---
+++ Challeges/DAY_01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+   
+ 
+    int arr[5] = {1, 3, 4, 6, 7};
+    for(int i = 1; i <= 5; i++)
+        cout << arr[i] << ' ';
+ 
+    return 0;
+}
+
+// what is the wrong in that code ???
