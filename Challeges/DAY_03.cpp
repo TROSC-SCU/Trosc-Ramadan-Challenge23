08239efd8e217ce1f691207c2a4f2c27cae2d5fc
TROSC تروس <128643049+TROSC-SCU@users.noreply.github.com>
2023-03-24 23:45:09 +0200

    DAY 3

---
+++ Challeges/DAY_03.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main(){
+  
+/*this code to get the prefix sum of elements in the array 
+prefix sum ==> every the element is equal the sum of itself and all previous elements 
+if you don't understand about it please google it :)
+*/
+ 
+int n;
+cin >> n;
+int arr[n];
+for (int i=0;i<n;i++)
+    cin >> arr[i];
+ 
+// prefix sum 
+for ( int i=0;i<n;i++) 
+     arr[i]+=arr[i-1];
+}
+
+// what is the wrong in that code ???
+
