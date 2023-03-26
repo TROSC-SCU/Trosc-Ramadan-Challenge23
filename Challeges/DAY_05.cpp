924676a7cf9511221413039eb5452e028cdf1260
TROSC تروس <128643049+TROSC-SCU@users.noreply.github.com>
2023-03-26 23:09:22 +0200

    DAY 5

---
+++ Challeges/DAY_05.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+// this code to get the binary representation of decimal number 
+ 
+string DecimaltoBinary(int num){
+    string Bin;
+    while(num){
+    Bin.push_back(num % 2);
+      num /= 2;
+    }
+         
+  
+    reverse(Bin.begin(), Bin.end());
+  
+    return Bin;
+}
+ 
+int main(){
+    int n;
+    cin >> n;
+    cout << DecimaltoBinary(n);
+    return 0;
+}
+
+// what is the wrong in the code ??
