fe3ed09fce847f363913b50795de4edb31bbcdbf
Omar <117028936+O-Abdelaziz@users.noreply.github.com>
2023-03-26 23:34:58 +0200

    Update DAY_05.cpp

--- Challeges/DAY_05.cpp
+++ Challeges/DAY_05.cpp
@@ -7,7 +7,7 @@ using namespace std;
 string DecimaltoBinary(int num){
     string Bin;
     while(num){
-    Bin.push_back(num % 2);
+    Bin.push_back((num % 2)+'0');
       num /= 2;
     }
          
@@ -25,3 +25,5 @@ int main(){
 }
 
 // what is the wrong in the code ??
+
+// Before adding the integer remainder to the binary string, we must convert it to a character, Adding the Ascii value 0 to the remaining values is one approach.
