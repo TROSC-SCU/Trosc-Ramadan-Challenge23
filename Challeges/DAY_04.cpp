f631e8e40da8f89fd42429cfebb344e89a30b75c
Omar <117028936+O-Abdelaziz@users.noreply.github.com>
2023-03-26 17:22:29 +0200

    Line no.7 syntax error

--- Challeges/DAY_04.cpp
+++ Challeges/DAY_04.cpp
@@ -4,12 +4,28 @@ using namespace std;
  
 int main(){
  
-0<= x <= 1e18
+//0<= x <= 1e18
  
     int x;
     cin >> x;
+    if (0 <= x && x <= 1e18) {
     cout << x;
+    } 
+    else{ 
+    cout << "x is out of range";
+    }
     return 0;
 
 }
 // what is the wrong in the code?
+
+/* In C++, the line "0<= x <= 1e18" is invalid syntax.
+To determine if x is inside the range, we should use an if statement. 
+The proper syntax using if statement is 
+    if (0 <= x && x <= 1e18) {
+    cout << x;
+    } 
+    else{ 
+    cout << "x is out of range";
+    }
+    */
