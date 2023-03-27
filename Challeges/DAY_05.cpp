057d2fcc1258dda1b5999700007e78d5a88e6c2f
NouranNoah <129011501+NouranNoah@users.noreply.github.com>
2023-03-27 02:21:38 +0200

    NouranNoah
    Ramadan Kareem

--- Challeges/DAY_05.cpp
+++ Challeges/DAY_05.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
- 
 using namespace std;
 
-// this code to get the binary representation of decimal number 
- 
+// this code to get the binary representation of decimal number
+
 string DecimaltoBinary(int num){
     string Bin;
+    string s;
+    int n=0;
     while(num){
-    Bin.push_back(num % 2);
+    s= to_string(num%2);
+    Bin.insert(n,s);
       num /= 2;
+      n++;
     }
-         
-  
+
+
     reverse(Bin.begin(), Bin.end());
-  
+
     return Bin;
 }
- 
+
 int main(){
     int n;
     cin >> n;
@@ -25,3 +28,4 @@ int main(){
 }
 
 // what is the wrong in the code ??
+// the wrong is that he used push_back function and this get the element in the last so when we use reverse function this make error.
