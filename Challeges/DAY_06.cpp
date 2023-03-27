9e18ec93a9897df5232888ae18b385a1902d4ad9
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-03-27 23:04:51 +0200

    DAY 6

---
+++ Challeges/DAY_06.cpp
@@ -0,0 +1,31 @@
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+/**
+* has cycle - function to determine if the linked list has a cycle in it.
+*@head: the head of a linkedlist
+*Return: true if there is a cycle in the linked list. 
+        Otherwise, return false.
+*/
+bool hasCycle(ListNode *head) {
+    ListNode* tmp = head;
+    map < ListNode*, bool > visited;
+    while(tmp){
+        // write your code here
+        
+    }
+    return 0;
+}
+ 
+/**
+*Constraints:
+ 
+The number of the nodes in the list is in the range [0, 104].
+-10^5 <= Node.val <= 10^5
+*/
+
+// what is the wrong in that code ???
