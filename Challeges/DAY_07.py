a021c65bd63794880920a66dbc72b6d17f5bc3bb
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-03-28 23:00:26 +0200

    DAY 7

---
+++ Challeges/DAY_07.py
@@ -0,0 +1,17 @@
+# funcation to check if th the input in  "Trosc" or not 
+
+def TroscCheck():
+    s = "Trosc"
+    c = input()
+    if c in s:
+    return True
+    else:
+    return False
+ 
+ 
+if TroscCheck():
+print("YES")
+else
+print("NO")
+ 
+# what is the wrong in the code???
