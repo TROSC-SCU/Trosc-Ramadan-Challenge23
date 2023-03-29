b1258f34b3f7ad5c9748d43b01464acd653a5539
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-03-30 00:33:43 +0200

    Complete Challenge of Day 8 and Solve issue ref #23

--- Challeges/DAY_08.cpp
+++ Challeges/DAY_08.cpp
@@ -4,18 +4,26 @@
 
 //An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
- bool isAnagram(string s, string t) {
-      // write your code here
-   
-   
-
-    }
+ bool isAnagram(const string& s, const string& t) {
+	// create two arrays of size 26 to store the frequency of each letter in the strings
+	vector<int> freq_s(26), freq_t(26); 
+	// increment the frequency of the letter in freq1 for each letter in s
+	for(auto& c : s)
+		freq_s[c - 'a']++;
+	// increment the frequency of the letter in freq2 for each letter in t
+	for(auto& c : t)
+		freq_t[c - 'a']++;
+	// compare the two frequency arrays, and return true if they are equal (i.e., s and t are anagrams)
+	return freq_s == freq_t;
+ }
 
 int main(){
-
-  string s,t;
-  cin >> s>>t;
-  if (  isAnagram( s,  t)) cout<<"YES";
-  else cout<<"NO";
-
+	string s, t;
+	cin >> s >> t;
+	// check if the two strings one of them is anagram of the another one
+	if (isAnagram(s, t))
+		cout << "YES\n";
+	else 
+		cout << "NO\n";
+	return 0;
 }
