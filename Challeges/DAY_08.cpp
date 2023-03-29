f4b8b2454fd5bcb621bd619aa51da5f7e567b158
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-03-30 00:04:53 +0200

    DAY 8

---
+++ Challeges/DAY_08.cpp
@@ -0,0 +1,21 @@
+ #include <bits/stdc++.h> 
+
+//Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ bool isAnagram(string s, string t) {
+      // write your code here
+   
+   
+
+    }
+
+int main(){
+
+  string s,t;
+  cin >> s>>t;
+  if (  isAnagram( s,  t)) cout<<"YES";
+  else cout<<"NO";
+
+}
