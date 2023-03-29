09c4ad60e24f8dae85c9503e3e0491ec97be3270
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-03-30 00:37:27 +0200

    Add using namespace to the code

--- Challeges/DAY_08.cpp
+++ Challeges/DAY_08.cpp
@@ -1,5 +1,7 @@
  #include <bits/stdc++.h> 
 
+using namespace std;
+
 //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 //An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
