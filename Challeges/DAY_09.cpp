78b6f96c798766dcacb67dddceb21a17b08d59b2
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-03-30 23:15:15 +0200

    DAY 9

---
+++ Challeges/DAY_09.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+ using namespace std;
+
+
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+    bool containsDuplicate(vector<int>& v) {
+        map<int,int>m;
+      
+// complete the code :)
+
+      
+        
+    }
+
+int main (){
+
+  int n;
+  cin >>n;
+  vector<int> v(n);
+  for ( auto & i:v) cin >> i;
+  
+  if (containsDuplicate(v))
+     cout<<"YES";
+  else 
+    cout<<"NO";
+  
+  
+  
+return 0;
+}
+
+
+
+
