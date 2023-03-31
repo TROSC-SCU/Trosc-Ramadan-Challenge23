0b10f4470d5d24183208503a2ab98d6787e31a4b
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-03-31 23:43:20 +0200

    Day 10

---
+++ Challeges/DAY_10.cpp
@@ -0,0 +1,45 @@
+ #include <bits/stdc++.h> 
+ using namespace std;
+
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+"(){}[]"  -> true
+"(())[()]"-> true
+"((())}"  -> false
+"[[]]("   -> false
+*/
+
+ bool isValid(string s) {
+        stack<char>st;
+
+       // complete the code : ) 
+   
+   
+   
+    }
+
+int main(){
+string s;
+  cin >> s;
+  if (isValid(s)){
+  cout<<"Valid"<<"\n";
+  }else {
+   cout<<"Not Valid"<<"\n";
+  }  
+
+return 0;
+}
+
+
+/*
+1 <= s.length <= 1e4
+s consists of parentheses only '()[]{}'.
+
+*/
