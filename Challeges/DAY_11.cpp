6dd98fefe45001a033496b43c57ec0c93804d4a2
Rofyda Elghadban <125513797+RofydaElghadban@users.noreply.github.com>
2023-04-02 00:37:33 +0200

    Update DAY_11.cpp

--- Challeges/DAY_11.cpp
+++ Challeges/DAY_11.cpp
@@ -1,12 +1,17 @@
  #include <bits/stdc++.h> 
-
+ #define ll long long
  using namespace std;
 
  // Check if three points in the same line
 
     bool is_same_line(ll x1, ll y1, ll x2, ll y2, ll x3, ll y3){
       // please complete the function  : )  
-      
+      ll res1,res2;
+      res1=((y3-y2)*(x2-x1));
+      res2=((y2-y1)*(x3-x2));
+      if(res1!=res2){
+          return false;
+      }return true;
     }
 
 int main (){
