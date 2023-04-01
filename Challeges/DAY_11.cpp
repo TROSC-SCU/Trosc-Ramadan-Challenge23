05773d49ef9511cf066a09559cffac89f990e80a
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-04-02 00:12:35 +0200

    Day 11

---
+++ Challeges/DAY_11.cpp
@@ -0,0 +1,23 @@
+ #include <bits/stdc++.h> 
+
+ using namespace std;
+
+ // Check if three points in the same line
+
+    bool is_same_line(ll x1, ll y1, ll x2, ll y2, ll x3, ll y3){
+      // please complete the function  : )  
+      
+    }
+
+int main (){
+ll x1,  y1,  x2,  y2, x3,  y3;
+  cin >>x1>>y1>>x2>>y2>>x3>>y3;
+  
+  if (is_same_line(x1, y1, x2, y2, x3, y3)){
+    cout<<"yes in the same line";
+  }else {
+  cout<<"not in the same line";
+  }
+  return 0;
+  
+}
