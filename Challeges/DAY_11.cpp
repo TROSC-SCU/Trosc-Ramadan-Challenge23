68351f1cf613bba7d246c30ec8bf84a8e90b2570
Omar <117028936+O-Abdelaziz@users.noreply.github.com>
2023-04-02 00:38:14 +0200

    Day 11

--- Challeges/DAY_11.cpp
+++ Challeges/DAY_11.cpp
@@ -2,12 +2,19 @@
 
  using namespace std;
 
+ typedef long long ll;
+
+
  // Check if three points in the same line
 
     bool is_same_line(ll x1, ll y1, ll x2, ll y2, ll x3, ll y3){
-      // please complete the function  : )  
+     
+      return (y2-y1)*(x3-x2) == (y3-y2)*(x2-x1);
+
+}
+
       
-    }
+    
 
 int main (){
 ll x1,  y1,  x2,  y2, x3,  y3;
