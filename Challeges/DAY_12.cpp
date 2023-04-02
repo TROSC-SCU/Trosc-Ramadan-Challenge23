ee58fa020d803cf4eeb16fdc4cbe4f723173fb3d
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-04-02 23:39:14 +0200

    Create DAY_12.cpp

---
+++ Challeges/DAY_12.cpp
@@ -0,0 +1,28 @@
+ #include <bits/stdc++.h> 
+
+ using namespace std;
+
+//Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+//An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+    bool isPowerOfTwo(int n) {
+      
+      // please compleate the code :)
+      
+      
+       
+    }
+      
+    
+
+int main (){
+int x;
+  cin >> x;
+  if (isPowerOfTwo(int n)==true) 
+    cout<<"This number is power of 2";
+  else 
+    cout<<"This number is not power of 2";
+  return 0;
+  
+}
