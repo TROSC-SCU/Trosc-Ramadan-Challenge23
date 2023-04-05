2bd251dde3378b88a288c5fe851d18d952525d9a
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2023-04-06 00:03:40 +0200

    Binary Search algorithm
    make it in O(log n)

---
+++ Challeges/Day_15.cpp
@@ -0,0 +1,15 @@
+/**
+* BS (Binary Search) - Find the first position of a target number in a sorted array.
+* Make it in O(log(n))
+* @nums: The integer array.
+* @target: Target to find.
+* Return: The first position of target. Position starts from 0.
+*       return -1 if target does not exist.
+*/
+
+
+int BS(vector<int& nums, int target)
+{
+  // write your code here
+    
+}
