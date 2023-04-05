29a12fdc960bafec18674226d0e63411e89526a3
Mohamed AbdElaal <74356112+AbdElaaal@users.noreply.github.com>
2023-04-06 01:09:22 +0200

    Update DAY_15.cpp

--- Challeges/DAY_15.cpp
+++ Challeges/DAY_15.cpp
@@ -8,8 +8,14 @@
 */
 
 
-int BS(vector<int& nums, int target)
+int BS(vector<int>& nums, int target)
 {
   // write your code here
-    
+  int l = 0, r = nums[nums.size() - 1];
+   while(l <= r){
+     int m = r - (r - l) / 2;
+     if(nums[m] == target) return m;
+     (nums[m] < target ? l = m + 1 : r = m - 1);
+   }
+   return -1;
 }
