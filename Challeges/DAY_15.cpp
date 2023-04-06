6f40214daef9b0857c8d0e2e77d0cfbb7c3a0b5c
Rofyda Elghadban <125513797+RofydaElghadban@users.noreply.github.com>
2023-04-06 22:15:07 +0200

    Update DAY_15.cpp

--- Challeges/DAY_15.cpp
+++ Challeges/DAY_15.cpp
@@ -11,5 +11,18 @@
 int BS(vector<int& nums, int target)
 {
   // write your code here
-    
+    sort(nums.begin(),nums.end());
+    int left=0,right=nums.size()-1,ans=-1;
+    while(left<=right){
+        int mid=left+((right-left)/2);
+        if (nums[mid] == target) {
+            ans=mid;
+            right=mid-1;
+        }else if(nums[mid]<target){
+            left=mid+1;
+        }else{
+            right=mid-1;
+        }
+    }
+    return ans;
 }
