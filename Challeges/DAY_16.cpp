01be54a5635157ecce449ac8de5b2b632980ac81
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2023-04-06 23:47:16 +0200

    Adding Binary Numbers

--- Challeges/DAY_16.cpp
+++ Challeges/DAY_16.cpp
@@ -4,7 +4,7 @@
 * a and b consist only of '0' or '1' characters.
 * Each string does not contain leading zeros except for the zero itself. */
 
-// you may need to know the rules binary numbers addition before start coding.
+// you may need to know the rules of binary numbers addition before start coding.
 string addBinary(string a, string b) {
       // write your code here.  
 }
