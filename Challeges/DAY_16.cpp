8044bd9acfdbe44f25f7a1d443e7a2a685e7f926
Rofyda Elghadban <125513797+RofydaElghadban@users.noreply.github.com>
2023-04-07 00:57:49 +0200

    Update DAY_16.cpp

--- Challeges/DAY_16.cpp
+++ Challeges/DAY_16.cpp
@@ -7,4 +7,17 @@
 // you may need to know the rules of binary numbers addition before start coding.
 string addBinary(string a, string b) {
       // write your code here.  
+        int i=a.length()-1,j=b.length()-1,carry=0;
+        string ans="";
+        while(i>=0||j>=0||carry){
+            if(i>=0){
+                carry+=a[i]-'0';
+                i--;
+            }if(j>=0){
+                carry+=b[j]-'0';
+                j--;
+            }ans+=carry%2+'0';
+            carry=carry/2;
+        }reverse(ans.begin(),ans.end());
+        return ans;
 }
