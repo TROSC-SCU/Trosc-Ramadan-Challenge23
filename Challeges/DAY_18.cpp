c893a1dc5fc588be46d89569e9a69949eb2e98ae
Rofyda Elghadban <125513797+RofydaElghadban@users.noreply.github.com>
2023-04-08 23:48:33 +0200

    Update DAY_18.cpp

--- Challeges/DAY_18.cpp
+++ Challeges/DAY_18.cpp
@@ -6,16 +6,16 @@
 
    void reverseString(string s) {
    // complete the function :)
-     
-     
-     
-    }
+     for(int i=s.size()-1;i>=0;i--){
+         cout<<s[i];
+     }
+   }
 
 int main(){
 string s;
-  cin >>s;
+cin >>s;
 reverseString(s);
-  cout<<s;
+  //cout<<s;
 
 return 0;
 }
