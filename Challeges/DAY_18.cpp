b66a3674e539f6715a386469cc206279b60d923c
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-04-08 23:33:25 +0200

    Create DAY_18.cpp

---
+++ Challeges/DAY_18.cpp
@@ -0,0 +1,21 @@
+  #include <bits/stdc++.h> 
+ 
+ using namespace std;
+ 
+// reverce the string without use reverce builtin function :)
+
+   void reverseString(string s) {
+   // complete the function :)
+     
+     
+     
+    }
+
+int main(){
+string s;
+  cin >>s;
+reverseString(s);
+  cout<<s;
+
+return 0;
+}
