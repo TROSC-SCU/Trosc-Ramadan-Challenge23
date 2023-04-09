5ff54b4ce8d51a45fa53c608f31db6cecef5462b
Rofyda Elghadban <125513797+RofydaElghadban@users.noreply.github.com>
2023-04-09 23:30:19 +0200

    Update DAY_19.cpp

--- Challeges/DAY_19.cpp
+++ Challeges/DAY_19.cpp
@@ -11,7 +11,12 @@
 
     int reverse(int x) {
         // complete the function 
-      
+      int dig,res=0;
+      while(x!=0){
+          dig=x%10;
+          res=(res*10)+dig;
+          x/=10;
+      }return res;
     }
 
 int main(){
