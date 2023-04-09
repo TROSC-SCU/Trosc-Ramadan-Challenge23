33177897b4c2b4d7e038cb29a65dfa9cd3450d89
Salma Ahmed Mostafa <56895305+salma234-del@users.noreply.github.com>
2023-04-10 00:09:27 +0200

    Update DAY_19.cpp

--- Challeges/DAY_19.cpp
+++ Challeges/DAY_19.cpp
@@ -11,7 +11,13 @@
 
     int reverse(int x) {
         // complete the function 
-      
+        int res = 0;
+        while(x){
+            res *= 10;
+            res += x % 10;
+            x /= 10;
+        }
+      return res;
     }
 
 int main(){
