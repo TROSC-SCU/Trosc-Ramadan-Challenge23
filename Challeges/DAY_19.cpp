f82203e78a681eedf74d57dab43e549174aa7545
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-04-09 23:08:35 +0200

    Create DAY_19.cpp

---
+++ Challeges/DAY_19.cpp
@@ -0,0 +1,24 @@
+ #include <bits/stdc++.h> 
+ using namespace std;
+
+// you are given an integer and want to reverse it :)
+
+// dont transfer the number to string it's not allowed
+
+// ex: 123 -> 321
+// ex: 120 -> 21 
+
+
+    int reverse(int x) {
+        // complete the function 
+      
+    }
+
+int main(){
+int n;
+  cin >>n;
+  int res=reverse(n);
+  cout<<res;
+  
+  return 0;
+}
