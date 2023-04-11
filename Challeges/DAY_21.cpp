3cbfdcae25493d0dc4366ca965ce7d4fef955c63
Mohamed AbdElaal <74356112+AbdElaaal@users.noreply.github.com>
2023-04-11 21:06:29 +0200

    Create DAY_21.cpp

---
+++ Challeges/DAY_21.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Given a number N Print the prime factors of N
+
+/*
+  Ex: 
+  N = 12
+  Prime factors of N = 2 * 2 * 3
+/*
+
+vector < int > factors;
+
+vector < int > prime_factorization(ll n){
+    
+}
+
+int main (){
+    ll n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+      cout << factors[i] << ' ';
+    return 0;
+}
