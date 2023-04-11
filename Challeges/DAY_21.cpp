48e85a4953cb4c6de1be9088151157a583f5540d
Mohamed AbdElaal <74356112+AbdElaaal@users.noreply.github.com>
2023-04-11 21:12:50 +0200

    Update DAY_21.cpp

--- Challeges/DAY_21.cpp
+++ Challeges/DAY_21.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 // Given a number N Print the prime factors of N
 
+// Prime Factorization is finding which prime numbers multiply together to make the original number.
+
 /*
   Ex: 
-  N = 12
-  Prime factors of N = 2 * 2 * 3
+  if the input number is 12, then the output should be “2 2 3”
 /*
 
-vector < int > factors;
+vector < int > prime_factors;
 
 vector < int > prime_factorization(ll n){
     
@@ -20,6 +21,6 @@ int main (){
     ll n;
     cin >> n;
     for(int i = 0; i < n; i++)
-      cout << factors[i] << ' ';
+      cout << Prime_factors[i] << ' ';
     return 0;
 }
