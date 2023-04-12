27a97adcb188dbaf9060c3ba18ecf0a5fbcdd422
Mohamed AbdElaal <74356112+AbdElaaal@users.noreply.github.com>
2023-04-12 21:50:08 +0200

    Create DAY_22.cpp

---
+++ Challeges/DAY_22.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    A cube number is a number multiplied by itself twice
+    Ex : 2³ = 2 × 2 × 2 = 8  
+    A square number is a number multiplied by itself
+    Ex : 2² = 2 x 2 = 4
+*/ 
+
+// For a given number n, count the number of cubes and squares from 1 to n
+
+set < int > ans;
+
+int Count(int n){
+    // Complete The Function 
+}
+
+int main (){
+    int n;
+    cin >> n;
+    cout << Count(n);
+    return 0;
+}
+    
