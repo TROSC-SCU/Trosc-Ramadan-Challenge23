8f70fbc20f67bc92bae69c36d4f412bb392dee7f
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-04-13 23:27:27 +0200

    Create DAY_23.cpp

---
+++ Challeges/DAY_23.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ex: 121   -> true
+ex: -121  ->false     -121 after reverse become 121- 
+
+note : not allowed to convert the integer to string :(
+
+*/
+
+
+    bool isPalindromeNumper(int x) {
+      // please complete the function  
+      
+      
+    }
+
+
+
+int main(){
+int n;
+  cin >> n;
+  if (isPalindromeNumper(n)) 
+    cout<<"Palindrome Number";
+  else 
+    cout<<"Is not Palindrome Number";
+  return 0;
+}
