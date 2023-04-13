7bfd8cf26fa7026c51b842477050f855a87ec242
Mohamed AbdElaal <74356112+AbdElaaal@users.noreply.github.com>
2023-04-13 23:21:37 +0200

    Create DAY_23.cpp

---
+++ Challeges/DAY_23.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// you have a string S each letter in it is ′L′or ′R′, find the maximum number of balanced strings by splitting the original string.
+
+// Balanced strings are those that have an equal quantity of ′L′and ′R′ characters.
+
+
+int Balanced_Strings(string s){
+    
+}
+
+
+int main (){
+    string s;
+    cin >> s;
+    cout << Balanced_Strings(s);
+    return 0;
+}
+
