e3fbc09c6378479d92b9baf61ea659461c824e50
Mazen Ghanayem <71566671+Mazen-Ghanaym@users.noreply.github.com>
2023-04-14 23:35:27 +0200

    Day_24

--- Challeges/DAY_24.cpp
+++ Challeges/DAY_24.cpp
@@ -8,7 +8,18 @@ using namespace std;
 
 
 int Balanced_Strings(string s){
+    int count=0,Balanced=0;
 
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == 'L')
+            count++;
+        else
+            count--;
+
+        if(count == 0)
+            Balanced++;
+    }
+    return Balanced;
 }
 
 
