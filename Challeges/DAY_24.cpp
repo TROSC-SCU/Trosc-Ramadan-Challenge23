d1f646c9e0cbaaa9c53d3a77710a8cfa0df9a3cf
Rofyda Elghadban <125513797+RofydaElghadban@users.noreply.github.com>
2023-04-15 12:59:43 +0200

    Update DAY_24.cpp

--- Challeges/DAY_24.cpp
+++ Challeges/DAY_24.cpp
@@ -8,7 +8,17 @@ using namespace std;
 
 
 int Balanced_Strings(string s){
-
+    int counter=0,res=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='R'){
+            counter++;
+        }else{
+            counter--;
+        }
+        if(counter==0){
+            res++;
+        }
+    }return res;
 }
 
 
