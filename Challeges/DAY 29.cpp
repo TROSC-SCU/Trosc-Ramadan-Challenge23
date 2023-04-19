22d55a4f6fa2cc395a0caf921ee68c11d379ce3b
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2023-04-20 00:14:35 +0200

    Create DAY 29.cpp

---
+++ Challeges/DAY 29.cpp
@@ -0,0 +1,10 @@
+/**
+* count_primes - a function that returns the number of prime numbers that are strictly less than n.
+* @n : that number to be compared
+* 0 <= n <= 5 * 106
+*/
+
+// write your algorithms here
+int countPrimes(int n) {
+  
+}
