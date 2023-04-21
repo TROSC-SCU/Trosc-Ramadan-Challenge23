4907880616c63e449c5698e7be6e5acaaa6c4789
Menna Zakaria <104664065+MennaAllahZakaria@users.noreply.github.com>
2023-04-21 02:38:46 +0200

    Create DAY_30.cpp

---
+++ Challeges/DAY_30.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+using namespace std;
+
+// HAPPY EID to you 
+// Because today is Eid elfeter it's our 3edia : )
+
+
+void HAPPY_EID(int n){
+  
+  // complete the function to white "HAPPY EID " n times :)
+  
+  
+  
+
+}
+
+int main (){
+int n;
+  cin >>n;
+  
+HAPPY_EID(n);
+  return 0;
+
+}
+
